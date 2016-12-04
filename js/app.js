af0b9b6b697bae6a0b8418914299f591f798dfc9
May Miller-Ricci <maymillerricci@gmail.com>
2016-12-03 20:15:03 -0800

    set up pug template for index page

--- js/app.js
+++ js/app.js
@@ -13,10 +13,19 @@ var config = require('../data/twitter_config');
 var twitter = new Twitter(config);
 
 // twitter.getUserTimeline({screen_name: 'maymillerricci', count: 10}, error, success);
+var path = require('path')
 
 var express = require('express');
 
 var app = express();
 
 app.set('view engine', 'pug');
-app.set('views', __dirname + '/templates');
+app.set('views', path.join(__dirname, '../views'));
+
+app.get('/', function(req, res) {
+  res.render("index");
+});
+
+app.listen(3000, function() {
+  console.log('The frontend server is running on port 3000.');
+})
