5ec835aaa4708b8637d8d1438942a499a7c35ce4
May Miller-Ricci <maymillerricci@gmail.com>
2016-12-12 22:40:23 -0800

    camel case

--- app.js
+++ app.js
@@ -34,27 +34,27 @@ app.listen(3000, function() {
 
 // make requests to twitter api getting all the data that's needed for the index page
 // and render index view passing in all the twitter data
-function renderIndexWithTwitterData(screen_name, res) {
+function renderIndexWithTwitterData(screenName, res) {
   
-  twitter.getUser({ screen_name: screen_name }, error, function(data) {
+  twitter.getUser({ screen_name: screenName }, error, function(data) {
     var user = JSON.parse(data);
 
-    twitter.getUserTimeline({ screen_name: screen_name, count: 5 }, error, function(data) {
+    twitter.getUserTimeline({ screen_name: screenName, count: 5 }, error, function(data) {
       var tweets = JSON.parse(data);
 
-      twitter.getCustomApiCall('/friends/list.json', { screen_name: screen_name, count: 5 }, error, function(data) {
+      twitter.getCustomApiCall('/friends/list.json', { screen_name: screenName, count: 5 }, error, function(data) {
         var friends = JSON.parse(data).users;
 
         twitter.getCustomApiCall('/direct_messages.json', { count: 3 }, error, function(data) {
-          var messages_to = JSON.parse(data);
+          var messagesTo = JSON.parse(data);
 
           twitter.getCustomApiCall('/direct_messages/sent.json', { count: 3 }, error, function(data) {
-            var messages_from = JSON.parse(data);
+            var messagesFrom = JSON.parse(data);
 
-            var messages = messages_to.concat(messages_from);
-            var sorted_messages = sortBy(messages, 'created_at');
+            var messages = messagesTo.concat(messagesFrom);
+            var sortedMessages = sortBy(messages, 'created_at');
 
-            res.render('index', { user: user, tweets: tweets, friends: friends, messages: sorted_messages });
+            res.render('index', { user: user, tweets: tweets, friends: friends, messages: sortedMessages });
 
           });
         });
