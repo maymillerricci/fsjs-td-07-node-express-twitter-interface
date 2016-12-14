fdd0776ae2e858706761ee86cc3acbacd1388c5a
May Miller-Ricci <maymillerricci@gmail.com>
2016-12-13 16:52:29 -0800

    show character count remianing for tweet and show red and disable if over limit

--- public/js/global.js
+++ public/js/global.js
@@ -5,6 +5,8 @@ $('.app--tweet form').on('submit', function(e) {
   e.preventDefault();
   var tweetText = $('#tweet-textarea').val();
   postTweet(tweetText);
+  // clear input field
+  $('#tweet-textarea').val('');
 });
 
 // send tweet text to /tweet post route to then make api call to post tweet and return tweet data
@@ -30,3 +32,18 @@ function prependTweet(tweet) {
     }
   });
 }
+
+// show number of characters remaining to 140 character limit for tweet as you type
+// if over limit, show negative number in red and disable submit button
+$('#tweet-textarea').on('keyup', function() {
+  var charCount = $(this).val().length;
+  var charsLeft = 140 - charCount;
+  $('#tweet-char').text(charsLeft);
+  if (charsLeft < 0) {
+    $('#tweet-char').addClass('over-char-limit');
+    $('.app--tweet button').attr('disabled', true);
+  } else {
+    $('#tweet-char').removeClass('over-char-limit');
+    $('.app--tweet button').attr('disabled', false);
+  }
+});
